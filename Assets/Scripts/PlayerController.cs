fbc3328a3a6524e25a24ffbb784644e266dde363
John <G00367771@gmit.ie>
2022-05-03 20:00:46 +0100

    Player sprite and animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private float moveInput;
-    private float speed = 25f;
+    private float speed = 10f;
 
 
 
@@ -16,11 +16,26 @@ public class PlayerController : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+
+        if(moveInput < 0)
+        {
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else 
+        {
+             this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         moveInput = Input.GetAxis("Horizontal");
         rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
         //rb2d.velocity = new Vector2(Input.acceleration.x * speed, rb2d.velocity.y);
+
+
     }
 }
