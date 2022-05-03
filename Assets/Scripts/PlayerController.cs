0bb8778b8269dbc9294aade77c956e056e8b1958
John <G00367771@gmit.ie>
2022-05-04 00:34:37 +0100

    Game Over scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private float moveInput;
     private float speed = 10f;
+
+    private float topScore = 0.0f;
+
+    public Text scoreText;
 
 
 
@@ -19,6 +25,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
+        if(rb2d.velocity.y > 0 && transform.position.y > topScore)
+        {
+            topScore = transform.position.y;
+        }
+
+        scoreText.text = Mathf.Round(topScore).ToString();
+
         if(moveInput < 0)
         {
             this.GetComponent<SpriteRenderer>().flipX = true;
@@ -27,20 +40,19 @@ public class PlayerController : MonoBehaviour
         {
              this.GetComponent<SpriteRenderer>().flipX = false;
         }
+
+         if(transform.position.y+50 < topScore)
+        {
+           Debug.Log("ded");
+           SceneManager.LoadScene(1);
+        } 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         moveInput = Input.GetAxis("Horizontal");
-<<<<<<< HEAD
-        //rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y); //Keyboard Input
-        rb2d.velocity = new Vector2(Input.acceleration.x * speed, rb2d.velocity.y); // Accelerometer + Gyroscope
-=======
-        rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
-        //rb2d.velocity = new Vector2(Input.acceleration.x * speed, rb2d.velocity.y);
-
-
->>>>>>> 3b397c82816594837143dd77962914feef82a451
+        rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y); //Keyboard Input
+        //rb2d.velocity = new Vector2(Input.acceleration.x * speed, rb2d.velocity.y); // Accelerometer + Gyroscope
     }
 }
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+        // Start is called before the first frame update
+    // == onClick Event Handlers ==
+    public void Start_OnClick()
+    {
+        SceneManager.LoadScene(0); // Make sure the scene is added in the Build Settings
+    }
+}
