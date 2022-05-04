6b0b83bcd84d2e1d9e5a9c96a56cd4acb83cbecb
David <g00362038@gmit.ie>
2022-05-04 18:26:56 +0100

    Swipe gestures implemented for player selection

--- Assets/Scripts/CharacterSelectionController.cs
+++ Assets/Scripts/CharacterSelectionController.cs
@@ -7,58 +7,74 @@ using UnityEditor;
 
 public class CharacterSelectionController : MonoBehaviour
 {
-   public SpriteRenderer sr;
-   public Animator am;
-   public List<Sprite> skins = new List<Sprite>();
+    public SpriteRenderer sr;
+    public Animator am;
+    public List<Sprite> skins = new List<Sprite>();
 
-   public List<RuntimeAnimatorController> animation = new List<RuntimeAnimatorController>();
-   private int selectedSkin = 0;
-   private int selectedAnimation = 0;
-   public GameObject playerskin;
-   //public GameObject playerAnimation;
+    public List<RuntimeAnimatorController> animation = new List<RuntimeAnimatorController>();
+    private int selectedSkin = 0;
+    private int selectedAnimation = 0;
+    public GameObject playerskin;
+    //public GameObject playerAnimation;
+    public SwipeDetection swipeControls;
 
-   public void NextOption()
-   {
-       selectedSkin = selectedSkin + 1;
-      
-       if (selectedSkin == skins.Count)
-       {
-           selectedSkin = 0;
-           //selectedAnimation = 0;
-       }
-       sr.sprite = skins[selectedSkin];
+    public void NextOption()
+    {
+        selectedSkin = selectedSkin + 1;
+
+        if (selectedSkin == skins.Count)
+        {
+            selectedSkin = 0;
+            //selectedAnimation = 0;
+        }
+        sr.sprite = skins[selectedSkin];
 
         selectedAnimation = selectedAnimation + 1;
 
         if (selectedAnimation == animation.Count)
-       {
-           selectedAnimation = 0;
-       }
+        {
+            selectedAnimation = 0;
+        }
         am.runtimeAnimatorController = animation[selectedAnimation];
-   }
+    }
 
     public void BackOption()
-   {
-       selectedSkin = selectedSkin - 1;
-        
-       if (selectedSkin < skins.Count)
-       {
-           selectedSkin = skins.Count - 1;
-           
-       }
-       sr.sprite = skins[selectedSkin];
-   
+    {
+        selectedSkin = selectedSkin - 1;
+
+        if (selectedSkin < skins.Count)
+        {
+            selectedSkin = skins.Count - 1;
+
+        }
+        sr.sprite = skins[selectedSkin];
+
         selectedAnimation = selectedAnimation - 1;
         if (selectedAnimation < animation.Count)
         {
             selectedAnimation = animation.Count - 1;
         }
         am.runtimeAnimatorController = animation[selectedAnimation];
-   }
+    }
 
-   public void PlayGame()
-   {
-       PrefabUtility.SaveAsPrefabAsset(playerskin, "Assets/selectedskin.prefab" );
-       SceneManager.LoadScene(1);
-   }
+    private void Update()
+    {
+        if (swipeControls.SwipeLeft)
+        {
+            BackOption();
+        }
+
+        if (swipeControls.SwipeRight)
+        {
+            NextOption();
+        }
+
+
+    }
+
+    public void PlayGame()
+    {
+        PrefabUtility.SaveAsPrefabAsset(playerskin, "Assets/selectedskin.prefab");
+        SceneManager.LoadScene(1);
+    }
 }
