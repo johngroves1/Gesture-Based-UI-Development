978bcd45f053d1da0de32215dd6d8ce7ad797c1b
David <g00362038@gmit.ie>
2022-05-04 05:26:30 +0100

    Pinch to scale player image added

---
+++ Assets/Scripts/PinchScale.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchScale : MonoBehaviour
+{
+    public float width = 1;
+    public float height = 1;
+    public Vector3 position = new Vector3(10, 5, 0);
+
+    public PinchScaleController pinch;
+
+    void Awake()
+    {
+        // set the scaling
+        Vector3 scale = new Vector3(width, height, 1f);
+        transform.localScale = scale;
+        // set the position
+        //transform.position = position;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
