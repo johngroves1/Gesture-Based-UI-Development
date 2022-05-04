2046b5f513fb3599098dcd305730bfacd421b583
John <G00367771@gmit.ie>
2022-05-04 02:23:33 +0100

    Star particles and menu clean

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if(isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    // Shows the pause menu and set time scale to 0
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+}
